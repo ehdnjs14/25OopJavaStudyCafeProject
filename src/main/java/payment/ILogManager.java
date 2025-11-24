e05deb5f71f8ca55c606c576924f686224d6b800
Kch310 <161842448+Kch310@users.noreply.github.com>
2025-11-24 17:00:02 +0900

    Update ILogManager.java

--- src/main/java/payment/ILogManager.java
+++ src/main/java/payment/ILogManager.java
@@ -4,4 +4,6 @@ import Seat.UsageSession;
 public interface ILogManager {
     void savePaymentLog(Payment payment);
     void saveUsageLog(UsageSession session);
+
+    void saveOrderLog(String logEntry); // 11/23 주문 로그 메서드
 }
