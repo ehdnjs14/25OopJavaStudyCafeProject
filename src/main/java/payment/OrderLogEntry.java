f00f226f668cc8d02b77d02a5736aca3d93d7c2c
ehdnjs14 <kdw023765@gmail.com>
2025-11-30 01:17:52 +0900

    Update OrderLogEntry.java

--- src/main/java/payment/OrderLogEntry.java
+++ src/main/java/payment/OrderLogEntry.java
@@ -1,13 +1,18 @@
-package payment;
+
+
+// 11/24 Payment/OrderLogEntry.java (새 파일)
+
+package Payment;
 
 public class OrderLogEntry {
     private String timestamp;
-    private String type = "ORDER";
+    private String type = "ORDER"; 
     private String memberId;
     private String seatNumber;
     private String detailedOrder;
     private long totalAmount;
 
+    // KioskMainFrame.java에서 LocalDateTime import가 필요할 수 있음
     public OrderLogEntry(String timestamp, String memberId, String seatNumber, String detailedOrder, long totalAmount) {
         this.timestamp = timestamp;
         this.memberId = memberId;
